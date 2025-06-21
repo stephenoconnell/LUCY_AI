acf91437cc5495308ea20155ebed3ef67b08d171
Stephen O'Connell <32414937+stephenoconnell@users.noreply.github.com>
2025-06-21 13:44:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.6.0/p5.min.js"></script>
+<script>
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+
+  function draw() {
+    background(0);
+    fill(255);
+    ellipse(mouseX, mouseY, 50, 50);
+  }
+</script>
 
